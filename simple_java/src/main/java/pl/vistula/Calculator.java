60829b21099c57a024187f25f51d4dd064ff72f1
yona-eng <81296285+yona-eng@users.noreply.github.com>
2022-04-02 14:17:39 +0200

    Add files via upload

---
+++ simple_java/src/main/java/pl/vistula/Calculator.java
@@ -0,0 +1,13 @@
+package pl.vistula;
+
+public interface Calculator {
+
+    public double add(double first, double second);
+
+    public double sub(double first, double second);
+
+    public double mul(double first, double second);
+
+    public double div(double first, double second);
+
+}
---
+++ simple_java/src/main/java/pl/vistula/Simplecalculator.java
@@ -0,0 +1,23 @@
+package pl.vistula;
+
+public class Simplecalculator implements Calculator {
+    @Override
+    public double add(double first, double second) {
+        return first + second;
+    }
+
+    @Override
+    public double sub(double first, double second) {
+        return first - second;
+    }
+
+    @Override
+    public double mul(double first, double second) {
+        return first * second;
+    }
+
+    @Override
+    public double div(double first, double second) {
+        return first / second ;
+    }
+}
